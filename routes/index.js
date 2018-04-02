26c31f0ee116205787b20529ca8e4a4043125264
petrvecera <vecera.petr@gmail.com>
2018-04-02 17:31:45 +0200

    Reformat code

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,8 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now())
   }
 })
-const multerFiles = multer({ 
-  storage : storage
+const multerFiles = multer({
+  storage: storage
 })
 
 // Transform bytes to human format
@@ -32,29 +32,29 @@ const sizeOf = function (bytes) {
 };
 
 // File type icons 
-function fileTypeIcon(type){
-  if(!type){
+function fileTypeIcon(type) {
+  if (!type) {
     return 'warning';
   }
-  if(type.includes('image') && !type.includes('gif')){
+  if (type.includes('image') && !type.includes('gif')) {
     return 'image';
   }
-  if(type.includes('x-msdos-program')){
+  if (type.includes('x-msdos-program')) {
     return 'play_circle_outline';
   }
-  if(type.includes('pdf')){
+  if (type.includes('pdf')) {
     return 'picture_as_pdf';
   }
-  if(type.includes('text')){
+  if (type.includes('text')) {
     return 'text_format';
   }
-  if(type.includes('gif')){
+  if (type.includes('gif')) {
     return 'gif';
   }
-  if(type.includes('video')){
+  if (type.includes('video')) {
     return 'ondemand_video';
   }
-  if(type.includes('zip')){
+  if (type.includes('zip')) {
     return 'archive';
   }
   return 'warning';
@@ -69,7 +69,7 @@ const _showFiles = (res) => {
       if (!fs.existsSync(uploadedFiles)) {
         fs.mkdirSync(uploadedFiles);
       }
-      
+
       fs.readdirSync(uploadedFiles).forEach(file => {
         files.push(file);
       });
@@ -105,83 +105,84 @@ const _showFiles = (res) => {
   }).then(files => {
 
     return new Promise((resolve, reject) => {
-      const filesAndFreeSpace = {}
+      const filesAndFreeSpace = {};
       filesAndFreeSpace.files = files;
-      filesAndFreeSpace.chartsInfo = {};      
+      filesAndFreeSpace.chartsInfo = {};
 
       const itemTypes = ['pdf', 'image', 'text', 'gif', 'video', 'exe', 'zip', 'other'];
 
-      for(let x of itemTypes){
-        filesAndFreeSpace.chartsInfo[x+'Count'] = 0;
-        filesAndFreeSpace.chartsInfo[x+'Size'] = 0;
-        filesAndFreeSpace.chartsInfo[x+'IntSize'] = 0;
+      for (let x of itemTypes) {
+        filesAndFreeSpace.chartsInfo[x + 'Count'] = 0;
+        filesAndFreeSpace.chartsInfo[x + 'Size'] = 0;
+        filesAndFreeSpace.chartsInfo[x + 'IntSize'] = 0;
       }
-      
-      filesAndFreeSpace.files.forEach(function typeSizeCalc(file){
-        if( file.icon == 'picture_as_pdf'){
-          filesAndFreeSpace.chartsInfo.pdfIntSize += file.intSize
+
+      filesAndFreeSpace.files.forEach(function typeSizeCalc(file) {
+        if (file.icon === 'picture_as_pdf') {
+          filesAndFreeSpace.chartsInfo.pdfIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.pdfCount += 1;
           filesAndFreeSpace.chartsInfo.pdfSize = sizeOf(filesAndFreeSpace.chartsInfo.pdfIntSize)
         }
-        else if(file.icon == 'image'){         
-          filesAndFreeSpace.chartsInfo.imageIntSize += file.intSize
+        else if (file.icon === 'image') {
+          filesAndFreeSpace.chartsInfo.imageIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.imageCount += 1;
           filesAndFreeSpace.chartsInfo.imageSize = sizeOf(filesAndFreeSpace.chartsInfo.imageIntSize)
         }
-        else if(file.icon == 'text_format'){         
-          filesAndFreeSpace.chartsInfo.textIntSize += file.intSize
+        else if (file.icon === 'text_format') {
+          filesAndFreeSpace.chartsInfo.textIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.textCount += 1;
           filesAndFreeSpace.chartsInfo.textSize = sizeOf(filesAndFreeSpace.chartsInfo.textIntSize)
         }
-        else if(file.icon == 'gif'){         
-          filesAndFreeSpace.chartsInfo.gifIntSize += file.intSize
+        else if (file.icon === 'gif') {
+          filesAndFreeSpace.chartsInfo.gifIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.gifCount += 1;
           filesAndFreeSpace.chartsInfo.gifSize = sizeOf(filesAndFreeSpace.chartsInfo.gifIntSize)
         }
-        else if(file.icon == 'ondemand_video'){          
-          filesAndFreeSpace.chartsInfo.videoIntSize += file.intSize
+        else if (file.icon === 'ondemand_video') {
+          filesAndFreeSpace.chartsInfo.videoIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.videoCount += 1;
           filesAndFreeSpace.chartsInfo.videoSize = sizeOf(filesAndFreeSpace.chartsInfo.videoIntSize)
         }
-        else if(file.icon == 'archive'){         
-          filesAndFreeSpace.chartsInfo.zipIntSize += file.intSize
+        else if (file.icon === 'archive') {
+          filesAndFreeSpace.chartsInfo.zipIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.zipCount += 1;
           filesAndFreeSpace.chartsInfo.zipSize = sizeOf(filesAndFreeSpace.chartsInfo.zipIntSize)
         }
-        else if(file.icon == 'play_circle_outline'){          
-          filesAndFreeSpace.chartsInfo.exeIntSize += file.intSize
+        else if (file.icon === 'play_circle_outline') {
+          filesAndFreeSpace.chartsInfo.exeIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.exeCount += 1;
           filesAndFreeSpace.chartsInfo.exeSize = sizeOf(filesAndFreeSpace.chartsInfo.exeIntSize)
         }
-        else if(file.icon == 'warning'){          
-          filesAndFreeSpace.chartsInfo.otherIntSize += file.intSize
+        else if (file.icon === 'warning') {
+          filesAndFreeSpace.chartsInfo.otherIntSize += file.intSize;
           filesAndFreeSpace.chartsInfo.otherCount += 1;
           filesAndFreeSpace.chartsInfo.otherSize = sizeOf(filesAndFreeSpace.chartsInfo.otherIntSize)
         }
-      })
+      });
 
 
-      diskspace.check(location, function (err, result){
-          if(err) {
-            reject(err);
-          } else {
-            filesAndFreeSpace.freeSpace = sizeOf(result.free);
-            filesAndFreeSpace.usedSpace = sizeOf(result.used);
-            filesAndFreeSpace.totalSpace = sizeOf(result.total);
-            filesAndFreeSpace.free = result.free;
-            filesAndFreeSpace.used = result.used;        
-            resolve(filesAndFreeSpace)
-          }        
-       });
-    });       
-  }).then((filesAndFreeSpace) => {     
-   res.render('index', {title: 'Upload', files: _.orderBy(filesAndFreeSpace.files, ['created'], ['desc']), 
-                                         freeSpace : filesAndFreeSpace.freeSpace, free : filesAndFreeSpace.free,
-                                         usedSpace : filesAndFreeSpace.usedSpace, used : filesAndFreeSpace.used,
-                                         totalSpace : filesAndFreeSpace.totalSpace,
-                                         sizeLocation: location,
-                                         json: filesAndFreeSpace.chartsInfo
-                                        });
+      diskspace.check(location, function (err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          filesAndFreeSpace.freeSpace = sizeOf(result.free);
+          filesAndFreeSpace.usedSpace = sizeOf(result.used);
+          filesAndFreeSpace.totalSpace = sizeOf(result.total);
+          filesAndFreeSpace.free = result.free;
+          filesAndFreeSpace.used = result.used;
+          resolve(filesAndFreeSpace)
+        }
+      });
+    });
+  }).then((filesAndFreeSpace) => {
+    res.render('index', {
+      title: 'Upload', files: _.orderBy(filesAndFreeSpace.files, ['created'], ['desc']),
+      freeSpace: filesAndFreeSpace.freeSpace, free: filesAndFreeSpace.free,
+      usedSpace: filesAndFreeSpace.usedSpace, used: filesAndFreeSpace.used,
+      totalSpace: filesAndFreeSpace.totalSpace,
+      sizeLocation: location,
+      json: filesAndFreeSpace.chartsInfo
+    });
 
   }).catch((err) => {
     res.render('index', {title: 'There was an error check console'});
@@ -196,13 +197,13 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.post('/',  multerFiles.single('sampleFile'), function(req, res) {
+router.post('/', multerFiles.single('sampleFile'), function (req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
 
   const sampleFile = req.files.sampleFile;
- 
-  sampleFile.mv(uploadedFiles + sampleFile.name, function(err) {
+
+  sampleFile.mv(uploadedFiles + sampleFile.name, function (err) {
     if (err)
       return res.status(500).send(err);
 
